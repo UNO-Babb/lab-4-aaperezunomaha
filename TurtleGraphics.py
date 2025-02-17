d04ca4ec569aa89c22daaeec9273373c28602139
aaperezunomaha <aaperez@unomaha.edu>
2025-02-16 18:18:28 -0600

    Update TurtleGraphics.py

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -13,11 +13,46 @@ def drawSquare(myTurtle, size):
 
 
 def main():
-    myTurtle = turtle.Turtle()
+    me = turtle.Turtle()
     # drawPolygon(myTurtle, 5) #draws a pentagon
+def main():
+    me = turtle.Turtle()
+    me.forward(100)
+    me.left(70)
+    me.forward(120)
+    me.left(70)
+    me.forward(150)
+    me.left(80)
+    me.forward(150)
+    me.left(70)
+    me.forward(120)
+    me.left(70)
+    me.forward(100)
     # drawPolygon(myTurtle, 8) #draws an octogon
+def main():
+    me2 = turtle.Turtle()
+    me2 = turtle.Turtle()
+    me2.forward(50)
+    me2.left(70)
+    me2.forward(50)
+    me2.left(20)
+    me2.forward(50)
+    me2.left(30)
+    me2.forward(50)
+    me2.left(60)
+    me2.forward(100)
+    me2.left(60)
+    me2.forward(50)
+    me2.left(30)
+    me2.forward(50)
+    me2.left(30)
+    me2.forward(55)
+    me2.left(60)
+    me2.forward(100)
+
 
     # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+
     # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
